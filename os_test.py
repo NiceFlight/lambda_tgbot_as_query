09d35de4397dc9d0d1ba52e55f14c95e2d7cc186
NiceFlight <167335404+NiceFlight@users.noreply.github.com>
2025-02-27 13:14:50 +0800

    Delete os_test.py

--- os_test.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-# token = os.environ.get("TELEGRAM_TOKEN")
-# token = os.environ["TELEGRAM_TOKEN"]
-token = os.getenv('TELEGRAM_TOKEN')
-print(token)
